4edcee1c51b527cf5d877182421249d2797869c9
Sushmita <sushmitakumari363@gmail.com>
2023-05-19 17:32:06 +0530

    login stylling assed

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,22 @@
 import Input from "@mui/material/Input";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import "./Login.css";
 
 export const Login = () => {
   return (
-    <div>
-      <label>
-        <Input placeholder="Enter your email" />
+    <div className="loginPage">
+      <h3>Login</h3>
+      <label className="inputLogin">
         Email
+        <Input placeholder="Enter your email" />
       </label>
-      <label>
+      <label className="inputLogin">
+        Password
         <Input placeholder="Enter password " />
       </label>
-      <Button>Login</Button>
-      <Link>create a new account</Link>
+      <Button className="loginBtn">Login</Button>
+      <Link className="signupLink">create a new account</Link>
     </div>
   );
 };
